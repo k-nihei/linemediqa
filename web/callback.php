4b7c3cc4f2837c84d97f1c933d9a02177f4f10f3
zedangate <estima.aeras.t.2.4@gmail.com>
2016-11-11 18:09:57 +0900

    Add files via upload

--- web/callback.php
+++ web/callback.php
@@ -1,22 +1,17 @@
 <?php
 $accessToken = getenv('LINE_CHANNEL_ACCESS_TOKEN');
-
-
 //ユーザーからのメッセージ取得
 $json_string = file_get_contents('php://input');
 $jsonObj = json_decode($json_string);
-
 $type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
 //メッセージ取得
 $text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
 //ReplyToken取得
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
-
 //メッセージ以外のときは何も返さず終了
 if($type != "text"){
 	exit;
 }
-
 //返信データ作成
 if ($text == 'はい') {
   $response_format_text = [
@@ -31,7 +26,7 @@ if ($text == 'はい') {
           [
             "type" => "message",
             "label" => "会員登録・ログイン方法",
-            "data" => "会員登録"
+            "text" => "会員登録"
           ],
           [
             "type" => "message",
@@ -53,6 +48,39 @@ if ($text == 'はい') {
   ];
 } else if ($text == 'いいえ') {
   exit;
+} else if ($text == '会員登録') {
+  $response_format_text = [
+    "type" => "template",
+    "altText" => "こちらの事項ですか?",
+    "template" => [
+      "type" => "buttons",
+      "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img1.jpg",
+      "title" => "会員登録・ログイン方法",
+      "text" => "こちらですか?",
+      "actions" => [
+          [
+            "type" => "message",
+            "label" => "会員登録したのに確認メールが届かない",
+            "text" => "確認メール"
+          ],
+          [
+            "type" => "message",
+            "label" => "携帯メールの受信設定の方法がわからない",
+            "text" => "携帯メール"
+          ],
+          [
+            "type" => "message",
+            "label" => "パスワードを忘れた／パスワードを変更したい",
+            "text" => "パスワード"
+          ],
+          [
+            "type" => "message",
+            "label" => "違うやつ",
+            "text" => "そのほか"
+          ]
+      ]
+    ]
+  ];
 } else if ($text == '他の事') {
   $response_format_text = [
     "type" => "template",
@@ -73,56 +101,56 @@ if ($text == 'はい') {
               [
                   "type" => "uri",
                   "label" => "問い合わせる（ブラウザ起動）",
-                  "uri" => "https://www.medilink-study.com/contact/" . $_SERVER['SERVER_NAME'] . "/"
+                  "uri" => "https://www.medilink-study.com/contact/"
               ],
               [
                   "type" => "uri",
                   "label" => "詳しく見る（ブラウザ起動）",
-                  "uri" => "https://www.medilink-study.com/user_data/about.php" . $_SERVER['SERVER_NAME'] . "/"
+                  "uri" => "https://www.medilink-study.com/user_data/about.php"
               ]
             ]
           ],
           [
             "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-2.jpg",
-            "title" => "▲▲レストラン",
+            "title" => "mediLinkの構成",
             "text" => "それともこちら？（２つ目）",
             "actions" => [
               [
                   "type" => "postback",
-                  "label" => "予約する",
+                  "label" => "見る",
                   "data" => "action=rsv&itemid=222"
               ],
               [
-                  "type" => "postback",
-                  "label" => "電話する",
-                  "data" => "action=pcall&itemid=222"
+                  "type" => "uri",
+                  "label" => "問い合わせる（ブラウザ起動）",
+                  "uri" => "https://www.medilink-study.com/contact/"
               ],
               [
                   "type" => "uri",
                   "label" => "詳しく見る（ブラウザ起動）",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
+                  "uri" => "https://www.medilink-study.com/user_data/about.php"
               ]
             ]
           ],
           [
             "thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/img2-3.jpg",
-            "title" => "■■レストラン",
+            "title" => "mediLinkアプリ 複数端末でのご利用について",
             "text" => "はたまたこちら？（３つ目）",
             "actions" => [
               [
                   "type" => "postback",
-                  "label" => "予約する",
-                  "data" => "action=rsv&itemid=333"
+                  "label" => "見る",
+                  "data" => "action=rsv&itemid=222"
               ],
               [
-                  "type" => "postback",
-                  "label" => "電話する",
-                  "data" => "action=pcall&itemid=333"
+                  "type" => "uri",
+                  "label" => "問い合わせる（ブラウザ起動）",
+                  "uri" => "https://www.medilink-study.com/contact/"
               ],
               [
                   "type" => "uri",
                   "label" => "詳しく見る（ブラウザ起動）",
-                  "uri" => "https://" . $_SERVER['SERVER_NAME'] . "/"
+                  "uri" => "https://www.medilink-study.com/user_data/about.php"
               ]
             ]
           ]
@@ -132,10 +160,10 @@ if ($text == 'はい') {
 } else {
   $response_format_text = [
     "type" => "template",
-    "altText" => "何かご用ですか？（はい／いいえ）",
+    "altText" => "こんにちわ 何かご用ですか？（はい／いいえ）",
     "template" => [
         "type" => "confirm",
-        "text" => "何かご用ですか？",
+        "text" => "こんにちわ 何かご用ですか？",
         "actions" => [
             [
               "type" => "message",
@@ -151,12 +179,10 @@ if ($text == 'はい') {
     ]
   ];
 }
-
 $post_data = [
 	"replyToken" => $replyToken,
 	"messages" => [$response_format_text]
 	];
-
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
